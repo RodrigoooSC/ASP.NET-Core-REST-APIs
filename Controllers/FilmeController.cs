34690e49ced77c1f9030d4b372846a7595210b28
Rodrigo Silva <rodrigooosc@outlook.com>
2022-06-25 19:57:47 -0300

    Configurando o Filmecontroller

--- Controllers/FilmeController.cs
+++ Controllers/FilmeController.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Linq;
+using FilmesAPI.Data;
 using FilmesAPI.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,15 +10,19 @@ namespace FilmesAPI.Controllers
     [Route("[Controller]")] // Define a rota da API neste caso a rota principal sera o nome da controller Filme
     public class FilmeController : ControllerBase
     {
-        private static List<Filme> filmes = new List<Filme>(); // Cria uma lista de filmes paar ser manipulada em tempo de execução
-        private static int id = 1; // Cria o id dos filmes
+
+        private FilmeContext _context;
+
+        public FilmeController(FilmeContext context)
+        {
+            _context = context;
+        }
 
 
         [HttpPost] // Adiciona um filme
         public IActionResult AdicionaFilme([FromBody] Filme filme)
         {
-            filme.Id = id++; // Adiciona um id ao filme
-            filmes.Add(filme); // Adiciona um filme na lista
+            _context.Filmes.Add(filme);
             // retorna o status code 201 (Created) e a localização de onde o recurso pode ser acessado no nosso sistema
             return CreatedAtAction(nameof(RecuperaFilmePorId), new { Id = filme.Id }, filme); 
         }
@@ -25,14 +30,14 @@ namespace FilmesAPI.Controllers
         [HttpGet] // Recupera todos os filmes
         public IActionResult RecuperaFilmes()
         {
-            return Ok(filmes); // retorna o status code 200 de success juntamente com a lista de filmes
+            return Ok(_context.Filmes); // retorna o status code 200 de success juntamente com a lista de filmes
         }
 
         [HttpGet("{id}")] // Recupera um filme por id
         public IActionResult RecuperaFilmePorId(int id) // recebe o parametro id do verbo HttpGet("{id}")
         {
             // retorna o primeiro resultado da busca
-            Filme filme = filmes.FirstOrDefault(filme => filme.Id == id); 
+            Filme filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id); 
             if (filme != null) 
             {
                 // caso a consulta não seja nula, retorno o status code 200 juntamente com o filme
