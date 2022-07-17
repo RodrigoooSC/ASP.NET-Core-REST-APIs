12d18c9066381d1d4dd263f7234b124e742d45b2
Rodrigo Silva <rodrigooosc@outlook.com>
2022-07-16 21:39:03 -0300

    Criando filtro nos cinemas

--- Controllers/CinemaController.cs
+++ Controllers/CinemaController.cs
@@ -12,7 +12,7 @@ using System.Threading.Tasks;
 namespace FilmesAPI.Controllers
 {
     [ApiController]
-    [Route("[controller]")] 
+    [Route("[controller]")]
     public class CinemaController : ControllerBase
     {
         private FilmeContext _context;
@@ -23,7 +23,7 @@ namespace FilmesAPI.Controllers
             _context = context;
             _mapper = mapper;
         }
-  
+
 
         [HttpPost]
         public IActionResult AdicionaCinema([FromBody] CreateCinemaDto cinemaDto)
@@ -35,16 +35,33 @@ namespace FilmesAPI.Controllers
         }
 
         [HttpGet]
-        public IEnumerable<Cinema> RecuperaCinemas([FromQuery] string nomeDoFilme)
+        public IActionResult RecuperaCinemas([FromQuery] string nomeDoFilme)
         {
-            return _context.Cinemas;
+            List<Cinema> cinemas = _context.Cinemas.ToList();
+            if (cinemas == null)
+            {
+                return NotFound();
+            }
+            if (!string.IsNullOrEmpty(nomeDoFilme)) // Se a lista não for nula e vazia executa a consulta
+            {
+                // Consulta sql que verifica os cinemas onde estão passado os filmes
+                IEnumerable<Cinema> query = from cinema in cinemas
+                                            where cinema.Sessoes.Any(sessao =>
+                                            sessao.Filme.Titulo == nomeDoFilme)
+                                            select cinema;
+
+                // Substitui a lista original pelo filtro da query
+                cinemas = query.ToList();
+            }
+            List<ReadCinemaDto> cinemaDto = _mapper.Map<List<ReadCinemaDto>>(cinemas);
+            return Ok(cinemaDto);
         }
 
         [HttpGet("{id}")]
         public IActionResult RecuperaCinemaPorId(int id)
         {
             Cinema cinema = _context.Cinemas.FirstOrDefault(cinema => cinema.Id == id);
-            if(cinema != null)
+            if (cinema != null)
             {
                 ReadCinemaDto cinemaDto = _mapper.Map<ReadCinemaDto>(cinema);
                 return Ok(cinemaDto);
@@ -56,7 +73,7 @@ namespace FilmesAPI.Controllers
         public IActionResult AtualizaCinema(int id, [FromBody] UpdateCinemaDto cinemaDto)
         {
             Cinema cinema = _context.Cinemas.FirstOrDefault(cinema => cinema.Id == id);
-            if(cinema == null)
+            if (cinema == null)
             {
                 return NotFound();
             }
