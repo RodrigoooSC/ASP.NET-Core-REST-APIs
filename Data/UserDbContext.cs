601e756d9058e73bedb9d370b41f58f760be7829
Rodrigo Silva <rodrigooosc@outlook.com>
2022-07-24 15:28:16 -0300

    Gerando migration -Criando Usuario- e ajustando projeto

--- Data/UserDbContext.cs
+++ Data/UserDbContext.cs
@@ -10,5 +10,27 @@ namespace UsuarioAPI.Data
         public UserDbContext(DbContextOptions<UserDbContext> options) : base(options)
         {            
         }
+
+        // protected override void OnModelCreating(ModelBuilder builder)
+        // {  
+        //     builder.Entity<IdentityUserLogin<int>>(entity => 
+        //     {
+        //         entity.Property(x => x.LoginProvider).HasMaxLength(110);
+        //         entity.Property(x => x.ProviderKey).HasMaxLength(127);
+        //     });
+
+        //     builder.Entity<IdentityUserRole<int>>(entity => 
+        //     {
+        //         entity.Property(m => m.UserId).HasMaxLength(127);
+        //         entity.Property(m => m.RoleId).HasMaxLength(127);
+        //     });
+
+        //     builder.Entity<IdentityUserToken<int>>(entity => 
+        //     {
+        //         entity.Property(m => m.UserId).HasMaxLength(110);
+        //         entity.Property(m => m.LoginProvider).HasMaxLength(110);
+        //         entity.Property(m => m.Name).HasMaxLength(110);
+        //     });        
+        // }
     }
 }
